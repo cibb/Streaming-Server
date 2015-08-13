7039bf03096b2a3e59da52f2884ecd4979658322
Pablo Galvan <pablo.galvan@olx.com>
2015-08-13 14:47:11 -0300

    another log

--- server/routers/app.js
+++ server/routers/app.js
@@ -30,6 +30,7 @@ module.exports = function route(app, callback) {
                     password: req.body.password
                 }
             }).on('complete', function(data, response) {
+                console.log(data);
                 if (response.statusCode == 200) {
                     app.set('session', true);
                     res.redirect('/play');
